63e576fb8c39aede3c7fd826f762791af177aa17
HAN TAE GYUN <kindkiz@naver.com>
2021-05-09 14:20:09 +0900

    feat: Update dictionary to parse multiple json files

--- Dictionary/dictionary.py
+++ Dictionary/dictionary.py
@@ -5,15 +5,17 @@ import string
 # Reference: National Institute of the Korean Language
 
 dictionary = {}
-with open(r'./NIKL Everyday Conversation corpus/SDRW2000000001.json', encoding='UTF8') as json_file:
-    json_data = json.load(json_file)
-    for utterance in json_data["document"][0]["utterance"]:
-        for words in utterance["form"].split(' '):
-            word = words.strip(string.punctuation)
-            if word in dictionary:
-                dictionary[word] += 1
-            else:
-                dictionary[word] = 1
+for tag in range(1, 200):
+    path = r"./NIKL Everyday Conversation corpus/SDRW200000" + format(tag, '04') + ".json"
+    with open(path, encoding='UTF8') as json_file:
+        json_data = json.load(json_file)
+        for utterance in json_data["document"][0]["utterance"]:
+            for words in utterance["form"].split(' '):
+                word = words.strip(string.punctuation)
+                if word in dictionary:
+                    dictionary[word] += 1
+                else:
+                    dictionary[word] = 1
 
 
 dictionary = dict(sorted(dictionary.items(), key=lambda x: x[1], reverse=True))
