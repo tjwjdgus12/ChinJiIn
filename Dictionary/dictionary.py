e10f6534c2e834575e00e87e4d5d986f02df6450
HAN TAE GYUN <kindkiz@naver.com>
2021-05-10 16:34:27 +0900

    fix: Remove words except for hangul

--- Dictionary/dictionary.py
+++ Dictionary/dictionary.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 import json
 import string
+import re
 # Building a dictionary of frequency words
 # Reference: National Institute of the Korean Language
 
+hangul = re.compile("[^ \u3131-\u3163\uac00-\ud7a3]+")
 dictionary = {}
 for tag in range(1, 400):
     path = r"./NIKL Everyday Conversation corpus/SDRW200000" + format(tag, '04') + ".json"
@@ -12,8 +14,7 @@ for tag in range(1, 400):
         for utterance in json_data["document"][0]["utterance"]:
             for words in utterance["form"].split(' '):
                 word = words.strip(string.punctuation).strip(string.digits)
-                if word == '' or word.encode().isalpha():
-                    continue
+                word = hangul.sub("", word)
                 if word in dictionary:
                     dictionary[word] += 1
                 else:
@@ -22,11 +23,12 @@ for tag in range(1, 400):
 
 dictionary = dict(sorted(dictionary.items(), key=lambda x: x[1], reverse=True))
 
+
 f = open('dictionary.txt', 'w')
 for key, value in dictionary.items():
     f.write(key + ": " + str(value) + "\n")
 f.close()
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     print(json.dumps(dictionary, indent='\t', ensure_ascii=False))
