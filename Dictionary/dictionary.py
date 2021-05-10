384f9085b1852a18294639eebb87d18dc49ebc31
HAN TAE GYUN <kindkiz@naver.com>
2021-05-10 13:04:25 +0900

    feat: Add test dictionary

--- Dictionary/dictionary.py
+++ Dictionary/dictionary.py
@@ -5,13 +5,13 @@ import string
 # Reference: National Institute of the Korean Language
 
 dictionary = {}
-for tag in range(1, 200):
+for tag in range(1, 400):
     path = r"./NIKL Everyday Conversation corpus/SDRW200000" + format(tag, '04') + ".json"
     with open(path, encoding='UTF8') as json_file:
         json_data = json.load(json_file)
         for utterance in json_data["document"][0]["utterance"]:
             for words in utterance["form"].split(' '):
-                word = words.strip(string.punctuation)
+                word = words.strip(string.punctuation).strip(string.digits)
                 if word in dictionary:
                     dictionary[word] += 1
                 else:
@@ -19,4 +19,11 @@ for tag in range(1, 200):
 
 
 dictionary = dict(sorted(dictionary.items(), key=lambda x: x[1], reverse=True))
-print(json.dumps(dictionary, indent='\t', ensure_ascii=False))
+
+f = open('dictionary.txt', 'w')
+for key, value in dictionary.items():
+    f.write(key + ": " + str(value) + "\n")
+f.close()
+
+if __name__ == "__main__":
+    print(json.dumps(dictionary, indent='\t', ensure_ascii=False))
