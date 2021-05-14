c42eaa3045ccd9234bee279cf9229526b967cc55
HAN TAE GYUN <kindkiz@naver.com>
2021-05-14 15:47:45 +0900

    feat: Add kor dictionary

---
+++ Dictionary/korean_word_list_dictionay.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from openpyxl import load_workbook
+import string
+import json
+# Vocabulary list for learning Korean
+# Reference: National Institute of the Korean Language
+load_wb = load_workbook('VocabularyList4LearningKor.xlsx', data_only=True)
+load_ws = load_wb["Sheet1"]
+dictionary = {}
+for i in load_ws['B1':'B5966']:
+    for cell in i:
+        word = cell.value.strip(string.digits)
+        if word in dictionary:
+            dictionary[word] += 1
+        else:
+            dictionary[word] = 1
+
+
+f = open('kor_dictionary.txt', "wt", encoding="utf-8")
+for key, value in dictionary.items():
+    f.write(key + ": " + str(value) + "\n")
+f.close()
+
+
+if __name__ == "__main__":
+    print(json.dumps(dictionary, indent='\t', ensure_ascii=False))
