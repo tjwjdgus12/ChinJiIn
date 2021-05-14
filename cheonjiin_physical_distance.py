a122ee90d38379558604ea35ef588b49c2d2c910
ChangminYi <jerry9803@gmail.com>
2021-05-14 10:54:28 +0900

    physical distance re-calculated, added documentation for calculation

--- cheonjiin_physical_distance.py
+++ cheonjiin_physical_distance.py
@@ -1,10 +1,21 @@
-conv_dict = {'ㅣ': 0, 'ㆍ': 1, 'ㅡ': 2, 'ㄱ': 3, 'ㄴ': 4, 'ㄷ': 5, 'ㅂ': 6, 'ㅅ': 7, 'ㅈ': 8, 'ㅇ': 9, '#': 10}
+avg_dist = float(1.566)
+convert_dict = {
+    'ㅣ': 0, 'ㆍ': 1, 'ㅡ': 2,
+    'ㄱ': 3, 'ㄴ': 4, 'ㄷ': 5,
+    'ㅂ': 6, 'ㅅ': 7, 'ㅈ': 8,
+    'ㅇ': 9, '#': 10
+    }
+distance_table = list()
+with open('./cheonjiin_physical_distance_table.txt', 'r') as f:
+    distance_table = [list(map(float, f.readline().split())) for i in range(len(convert_dict))]
 
-def getConvertTable():
-    f = open('./cheonjiin_physical_distance_table.txt', 'rt')
-    conv_table = [list(map(int, f.readline().split())) for i in range(len(conv_dict))]
-    f.close()
-    return conv_table
+
+def getPhysicalDist(origin, typo):
+    if len(origin) != 1 or len(typo) != 1:
+        return int(-1)
+    else:
+        return distance_table[convert_dict[origin]][convert_dict[typo]]
 
 
-
+def getAvgPhysicalDist():
+    return avg_dist
