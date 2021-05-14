4785cf0ffb8f1a373660324c1415952af7e17b69
ChangminYi <jerry9803@gmail.com>
2021-05-14 18:40:49 +0900

    Error in physical distance calculation in replacement fixed

--- cheonjiin_physical_distance.py
+++ cheonjiin_physical_distance.py
@@ -24,7 +24,8 @@ def getAvgPhysicalDist():
 
 
 # Damerau-Levenshtein distance based function: adjusted to calculate physical distance
-def getPhysEditDist(str_origin, str_typo):
+def getPhysEditDist(str1, str2):
+    """
     row, col = len(str_typo) + 1, len(str_origin) + 1
     table = [[float(0) for i in range(col)] for j in range(row)]
 
@@ -36,6 +37,13 @@ def getPhysEditDist(str_origin, str_typo):
         table[0][i] = table[0][i - 1] + getPhysicalDist(str_origin[i - 2], str_origin[i - 1])
     for i in range(2, len(str_typo) + 1):
         table[i][0] = table[i - 1][0] + getPhysicalDist(str_typo[i - 2], str_typo[i - 1])
+
+    # test
+    print()
+    for lst in table:
+        for i in lst:
+            print('%.2f' % i, end = '\t')
+        print()
     
     for i in range(1, row):
         for j in range(1, col):
@@ -49,5 +57,39 @@ def getPhysEditDist(str_origin, str_typo):
                 temp4 = table[i - 2][j - 2] + int(1)    # 이 부분도 평균값으로 바꿔야 하나?
             
             table[i][j] = min(temp1, temp2, temp3, temp4)
-        
+
+    # test
+    print()
+    for lst in table:
+        for i in lst:
+            print('%.2f' % i, end = '\t')
+        print()
+
     return table[row - 1][col - 1]
+    """
+    table = dict()
+    len_str1, len_str2 = len(str1), len(str2)
+    table[(-1, -1)] = 0
+    table[(-1, 0)], table[0, -1] = getPhysicalDist(str1[0], str2[0]), getPhysicalDist(str1[0], str2[0])
+    for i in range(0, len_str1):   # len_str1 + 1):
+        table[(i, -1)] = table[(i - 1, -1)] + getPhysicalDist(str1[i], str1[i - 1]) # i + 1
+    for j in range(0, len_str2):   # len_str2 + 1):
+        table[(-1, j)] = table[(-1, j - 1)] + getPhysicalDist(str2[j], str2[j - 1]) # j + 1
+
+    for i in range(len_str1):
+        for j in range(len_str2):
+            if str1[i] == str2[j]:
+                cost = float(0) # getPhysicalDist(str1[i], str2[j])
+            else:
+                cost = getPhysicalDist(str1[i], str2[j]) # float(1) # getAvgPhysicalDist()
+            table[(i, j)] = min(
+                           table[(i - 1, j)] + (table[(i, -1)] - table[(i - 1, -1)]), # 1, # deletion
+                           table[(i, j - 1)] + (table[(-1, j)] - table[(-1, j - 1)]), # 1, # insertion
+                           table[(i - 1, j - 1)] + cost # substitution
+                          )
+
+            if i and j and str1[i] == str2[j - 1] and str1[i - 1] == str2[j]:
+                table[(i, j)] = min(table[(i, j)], table[i - 2,j - 2] + cost) # transposition
+
+    return table[len_str1 - 1,len_str2 - 1]
+
