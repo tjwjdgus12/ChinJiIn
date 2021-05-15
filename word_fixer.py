5eb5f5b3b657bbbdae8bf52d67e15d15cabc18f8
tjwjdgus12 <01048733438a@gmail.com>
2021-05-15 18:06:23 +0900

    modules and codes are beautified and optimized

--- word_fixer.py
+++ word_fixer.py
@@ -1,7 +1,5 @@
-import cheonjiin_convert as cji_converter
-import deletes_convert as del_converter
-import hangeul_convert as han_converter
-import cheonjiin_physical_distance as cjiPhys
+import converter
+import measurer.edit_distance_measurer as measurer
 
 def load_origin_dict(inputFile):
     _origin_dict = dict()
@@ -19,28 +17,28 @@ def fix(input_word):
     global cji_dict
     
     origin_input = input_word
-    input_word = cji_converter.cheonjiin_convert(input_word)
+    input_word = converter.cji_converter.convert(input_word)
 
     ret = set()
 
     if input_word in cji_dict.keys():
-        ret.add(han_converter.hangeul_convert(input_word))
+        ret.add(converter.han_converter.convert(input_word))
         print('단어사전에 입력 키워드가 있는 예시', input_word)
 
     if input_word in delete_dict.keys():
         for keyword in delete_dict[input_word]:
-            ret.add(han_converter.hangeul_convert(keyword))
+            ret.add(converter.han_converter.convert(keyword))
             print('단어사전 del에 입력 키워드가 있는 예시', keyword)
 
     for input_word_del in del_converter.deletes(input_word):
         if input_word_del in cji_dict.keys():
-            ret.add(han_converter.hangeul_convert(input_word_del))
+            ret.add(converter.han_converter.convert(input_word_del))
             print('단어사전에 입력 키워드 del가 있는 예시', input_word_del)
 
     for input_word_del in del_converter.deletes(input_word):
         if input_word_del in delete_dict.keys():
             for keyword in delete_dict[input_word_del]:
-                ret.add(han_converter.hangeul_convert(keyword))
+                ret.add(converter.han_converter.convert(keyword))
                 print('단어사전 del에 입력 키워드 del가 있는 예시', keyword)
 
     print_arr = []
@@ -61,13 +59,13 @@ def fix(input_word):
 
     for r in print_arr:
         print('단어: ' + r[0] + ')', end = ' ')
-        print('물리적거리: %.2f' % cjiPhys.getPhysEditDist(input_word, cji_converter.cheonjiin_convert(r[0])), end = ' ')
+        print('물리적거리: %.2f' % measurer.getPhysEditDist(input_word, cji_converter.cheonjiin_convert(r[0])), end = ' ')
         print('빈도수: ', r[1])
     print("------------------")
 
 
 if __name__ == '__main__':
-    dictionaryFileName = "dict.txt"
+    dictionaryFileName = "converter/dict/mungchi_dict.txt"
     origin_dict = load_origin_dict(dictionaryFileName)
     cji_dict = cji_converter.load_cji_dict(dictionaryFileName)
     delete_dict = del_converter.makeDeleteDict(dictionaryFileName)
