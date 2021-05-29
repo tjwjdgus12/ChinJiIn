be0c4e47ee143d57fe222f19e96e230950150cbe
ChangminYi <jerry9803@gmail.com>
2021-05-29 22:42:34 +0900

    edit_distance_calculator.py refactored based on PEP8

--- chinjiin/measurer/edit_distance_calculater.py
+++ chinjiin/measurer/edit_distance_calculater.py
@@ -1,6 +1,8 @@
 import sys
 
-avg_dist = float(1.61)
+
+NON_CJI = int(11)       # index value of non-cheonjiin input
+AVERAGE_DISTANCE = float(1.61)  # average distance of all possible distance
 convert_dict = {
     'ㅣ': 0, 'ᆞ': 1, 'ㅡ': 2,
     'ㄱ': 3, 'ㄴ': 4, 'ㄷ': 5,
@@ -9,7 +11,8 @@ convert_dict = {
     }
 distance_table = list()
 with open('measurer/cji_physical_distance_table.txt', 'r') as f:
-    distance_table = [list(map(float, f.readline().split())) for i in range(len(convert_dict))]
+    for i in range(len(convert_dict)):
+        distance_table.append(list(map(float, f.readline().split())))
 
 
 def get_conv_dict(alphabet):
@@ -19,44 +22,56 @@ def get_conv_dict(alphabet):
         return convert_dict['ELSE']
 
 
-def get_physical_dist(origin, typo):
+def get_phys_dist(origin, typo):
     if len(origin) != 1 or len(typo) != 1:
         return int(-1)
-    elif origin == 11 or typo == 11:
-        return 1
+    elif origin == NON_CJI or typo == NON_CJI:
+        return int(1)
     else:
-        return distance_table[get_conv_dict(origin)][get_conv_dict(typo)]
+        ind_origin = get_conv_dict(origin)
+        ind_typo = get_conv_dict(typo)
+        return distance_table[ind_origin][ind_typo]
 
 
-def get_avg_physical_dist():
-    return avg_dist
+def get_avg_dist():
+    return AVERAGE_DISTANCE
 
 
 # Damerau-Levenshtein distance based function: adjusted to calculate physical distance
 def calc_edit_dist(str_origin, str_typo):
-    row, col = len(str_typo) + 1, len(str_origin) + 1
+    row = len(str_typo) + 1
+    col = len(str_origin) + 1
     table = [[float(0) for i in range(col)] for j in range(row)]
 
-    # 초기값: 물리적 거리 평균을 초기값으로 사용 (변경 가능)
-    # 원래 편집 거리 알고리즘에서는 모든 거리가 1이지만,
-    # 친지인에서는 물리적 거리를 고려하므로 distance_table의 값을 사용
-    table[0][1], table[1][0] = get_avg_physical_dist(), get_avg_physical_dist()
+    # initial data: average physical distance (cf. Documentation)
+    # in original algorithm, all distance is 1,
+    # but in chinjiin, it considers physical distance.
+    # So, getting distance values refer to distance table.
+    table[0][1] = get_avg_dist()
+    table[1][0] = get_avg_dist()
     for i in range(2, len(str_origin) + 1):
-        table[0][i] = table[0][i - 1] + get_physical_dist(str_origin[i - 2], str_origin[i - 1])
+        table[0][i] = (table[0][i - 1]
+                       + get_phys_dist(str_origin[i - 2], str_origin[i - 1]))
     for i in range(2, len(str_typo) + 1):
-        table[i][0] = table[i - 1][0] + get_physical_dist(str_typo[i - 2], str_typo[i - 1])
+        table[i][0] = (table[i - 1][0]
+                       + get_phys_dist(str_typo[i - 2], str_typo[i - 1]))
     
     for i in range(1, row):
         for j in range(1, col):
-            temp1 = table[i][j - 1] + (table[0][j] - table[0][j - 1])
-            temp2 = table[i - 1][j] + (table[i][0] - table[i - 1][0])
+            temp1 = (table[i][j - 1]
+                     # getting distance between adjacent two alphabet
+                     + (table[0][j] - table[0][j - 1]))
+            temp2 = (table[i - 1][j]
+                     # same with above comment
+                     + (table[i][0] - table[i - 1][0]))
             temp3 = table[i - 1][j - 1]
-            if str_typo[i - 1] != str_origin[j - 1]:
-                temp3 += int(1) # 물리적 거리 평균값으로 해야 하나?
             temp4 = sys.maxsize
+            if str_typo[i - 1] != str_origin[j - 1]:
+                temp3 += int(1)
             if i > 1 and j > 1:
-                temp4 = table[i - 2][j - 2] + int(1)    # 이 부분도 평균값으로 바꿔야 하나?
+                temp4 = table[i - 2][j - 2] + int(1)
             
-            table[i][j] = min(temp1, temp2, temp3, temp4)
+            table[i][j] = min(temp1, temp2,
+                              temp3, temp4)
         
     return table[row - 1][col - 1]
