931debba2b1b3495701b2adc8b355b81d59eddaa
ChangminYi <jerry9803@gmail.com>
2021-05-31 00:42:30 +0900

    Improvement of physical distance: substitution and transposition bug fix

--- chinjiin/measurer/edit_distance_calculater.py
+++ chinjiin/measurer/edit_distance_calculater.py
@@ -1,17 +1,17 @@
 import sys
 
-
-NON_CJI = int(11)       # index value of non-cheonjiin input
+INF = float(sys.maxsize)
+NON_CJI = int(11)  # index value of non-cheonjiin input
 AVERAGE_DISTANCE = float(1.61)  # average distance of all possible distance
 convert_dict = {
     'ㅣ': 0, 'ᆞ': 1, 'ㅡ': 2,
     'ㄱ': 3, 'ㄴ': 4, 'ㄷ': 5,
     'ㅂ': 6, 'ㅅ': 7, 'ㅈ': 8,
     'ㅇ': 9, '#': 10, 'ELSE': 11
-    }
+}
 distance_table = list()
 with open('measurer/cji_physical_distance_table.txt', 'r') as f:
-    for i in range(len(convert_dict)):
+    for cnt in range(len(convert_dict)):
         distance_table.append(list(map(float, f.readline().split())))
 
 
@@ -37,41 +37,67 @@ def get_avg_dist():
     return AVERAGE_DISTANCE
 
 
-# Damerau-Levenshtein distance based function: adjusted to calculate physical distance
-def calc_edit_dist(str_origin, str_typo):
-    row = len(str_typo) + 1
-    col = len(str_origin) + 1
-    table = [[float(0) for i in range(col)] for j in range(row)]
+def get_str_phys_dist(args):
+    total_len = float(0)
+    for i in range(1, len(args)):
+        total_len += get_phys_dist(args[i], args[i - 1])
+    return total_len
 
-    # initial data: average physical distance (cf. Documentation)
-    # in original algorithm, all distance is 1,
-    # but in chinjiin, it considers physical distance.
-    # So, getting distance values refer to distance table.
-    table[0][1] = get_avg_dist()
-    table[1][0] = get_avg_dist()
-    for i in range(2, len(str_origin) + 1):
-        table[0][i] = (table[0][i - 1]
-                       + get_phys_dist(str_origin[i - 2], str_origin[i - 1]))
-    for i in range(2, len(str_typo) + 1):
-        table[i][0] = (table[i - 1][0]
-                       + get_phys_dist(str_typo[i - 2], str_typo[i - 1]))
+
+# Damerau-Levenshtein distance based function: adjusted to calculate physical distance
+def calc_edit_dist(str_ori, str_typ):
+    # "Infinity" -- greater than maximum possible edit distance
+    # Used to prevent transpositions for first characters
+
+    print(str_ori, str_typ)
     
-    for i in range(1, row):
-        for j in range(1, col):
-            temp1 = (table[i][j - 1]
-                     # getting distance between adjacent two alphabet
-                     + (table[0][j] - table[0][j - 1]))
-            temp2 = (table[i - 1][j]
-                     # same with above comment
-                     + (table[i][0] - table[i - 1][0]))
-            temp3 = table[i - 1][j - 1]
-            temp4 = sys.maxsize
-            if str_typo[i - 1] != str_origin[j - 1]:
-                temp3 += int(1)
-            if i > 1 and j > 1:
-                temp4 = table[i - 2][j - 2] + int(1)
-            
-            table[i][j] = min(temp1, temp2,
-                              temp3, temp4)
-        
-    return table[row - 1][col - 1]
+    # table: (M + 2) x (N + 2) sized matrix
+    table = [[INF for _ in range(len(str_typ) + 2)] for __ in range(len(str_ori) + 2)]
+    table[1][1] = 0
+    table[2][1] = get_avg_dist()
+    table[1][2] = get_avg_dist()
+    for i in range(3, len(str_ori) + 2):
+        table[i][1] = table[i - 1][1] + get_phys_dist(str_ori[i - 2], str_ori[i - 3])
+    for i in range(3, len(str_typ) + 2):
+        table[1][i] = table[1][i - 1] + get_phys_dist(str_typ[i - 2], str_typ[i - 3])
+
+    # Holds last row each element was encountered: DA in the Wikipedia pseudocode
+    last_row = {}
+
+    # Fill in costs
+    for i in range(1, len(str_ori) + 1):
+        # Current character in a
+        ch_str_ori = str_ori[i - 1]
+
+        # Column of last match on this row: DB in pseudocode
+        last_match_col = 0
+
+        for j in range(1, len(str_typ) + 1):
+            # Current character in b
+            ch_str_typ = str_typ[j - 1]
+
+            # Last row with matching character
+            last_match_row = last_row.get(ch_str_typ, 0)
+
+            # Cost of substitution
+            cost = float(0) if ch_str_ori == ch_str_typ else float(1)  # else some_value (치환에 대한 가중치)
+
+            # Compute substring distance
+            val_sub = table[i][j] + cost  # Substitution
+            val_add = table[i + 1][j] + (table[1][j + 1] - table[1][j])  # Addition
+            val_del = table[i][j + 1] + (table[i + 1][1] - table[i][1])  # Deletion
+            val_trs = (table[last_match_row][last_match_col]  # Transposition
+                       + max((i - last_match_row - 1), (j - last_match_col - 1))
+                       + float(1))
+            table[i + 1][j + 1] = min(val_sub, val_add,
+                                      val_del, val_trs)
+
+            # If there was a match, update last_match_col
+            if cost == 0:
+                last_match_col = j
+
+        # Update last row for current character
+        last_row[ch_str_ori] = i
+
+    # Return last element
+    return table[-1][-1]
