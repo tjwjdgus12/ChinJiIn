3c99b3b6c23fcc56750ce1d59781037eb6a1037e
tjwjdgus12 <01048733438a@gmail.com>
2021-05-30 19:23:24 +0900

    restyle word_fixer and del_converter a little, and code optimized

--- chinjiin/word_fixer.py
+++ chinjiin/word_fixer.py
@@ -4,60 +4,59 @@ from measurer import edit_distance_calculater
 DICTIONARY = 'mungchi_dict'
 RESET_ON_EVERY_EXECUTION = False
 
+
 def fix(input_word):
-    global del_dict, cji_dict
-    
     input_word = cji_converter.convert(input_word)
+    candidates = get_candidates(input_word)
+    print_candidates(sorted_candidates(input_word, candidates))
 
+
+def get_candidates(input_word):
     ret = set()
-
+    
     if input_word in cji_dict.keys():
         ret.add(input_word)
-        print('단어사전에 입력 키워드가 있는 예시', input_word)
+        #print('단어사전에 입력 키워드가 있는 예시', input_word)
 
     if input_word in del_dict.keys():
         for keyword in del_dict[input_word]:
-            if keyword in ret:
-                continue
             ret.add(keyword)
-            print('단어사전 del에 입력 키워드가 있는 예시', keyword)
+            #print('단어사전 del에 입력 키워드가 있는 예시', keyword)
 
     for input_word_del in del_converter.deletes(input_word):
         if input_word_del in cji_dict.keys():
-            if input_word_del in ret:
-                continue
             ret.add(input_word_del)
-            print('단어사전에 입력 키워드 del가 있는 예시', input_word_del)
+            #print('단어사전에 입력 키워드 del가 있는 예시', input_word_del)
 
     for input_word_del in del_converter.deletes(input_word):
         if input_word_del in del_dict.keys():
             for keyword in del_dict[input_word_del]:
-                if keyword in ret:
-                    continue
                 ret.add(keyword)
-                print('단어사전 del에 입력 키워드 del가 있는 예시', keyword)
+                #print('단어사전 del에 입력 키워드 del가 있는 예시', keyword)
 
+    return ret
+
+
+def sorted_candidates(input_word, candidates):
     print_arr = []
-    has_same = False
-    for i in ret:
-        if i in cji_dict:
-            if i == input_word:
-                has_same = True
-                continue
-            else:
-                temp = [i, 1 - (cji_dict[i] / 54868), edit_distance_calculater.calc_edit_dist(input_word, i)]
-        else:
-            temp = [i, int(1), edit_distance_calculater.calc_edit_dist(input_word, i)]
-        print_arr.append(temp)
-    print_arr.sort(key=lambda freq: (freq[1] / 2) + freq[2])
     
-    if has_same:
-        print_arr.insert(0, [input_word, (1 - cji_dict[input_word] / 54868), 0])
+    for cand in candidates:
+        temp = [
+            cand,
+            1 - (cji_dict[cand] / 54868),
+            edit_distance_calculater.calc_edit_dist(input_word, cand)
+        ]
+        print_arr.append(temp)
+        
+    print_arr.sort(key = lambda freq: (freq[1] / 2) + freq[2])
+    return print_arr
 
-    for r in print_arr:
-        print('교정단어: %s' % han_converter.convert(r[0]), end = ' ')
-        print('물리적 편집거리: %.2f' % r[2], end = ' ')
-        print('정규화된 빈도수: ', r[1])
+
+def print_candidates(candidates):
+    for cand in candidates:
+        print('교정단어: %s' % han_converter.convert(cand[0]), end = ' ')
+        print('물리적 편집거리: %.2f' % cand[2], end = ' ')
+        print('정규화된 빈도수: %.6f' % cand[1])
     print("------------------")
 
 
@@ -68,5 +67,4 @@ if __name__ == '__main__':
     del_dict = del_converter.load_del_dict(DICTIONARY)
  
     while True:
-        print("Input:", end = ' ')
-        fix(input())
+        fix(input("Input: "))
