641a3756b1dad219d7a3c67a3a1dd87806810cc1
ChangminYi <jerry9803@gmail.com>
2021-06-07 18:06:15 +0900

    Too long statment has been shortened

--- chinjiin/measurer/edit_distance_calculater.py
+++ chinjiin/measurer/edit_distance_calculater.py
@@ -1,6 +1,5 @@
 import sys
 
-
 INF = float(sys.maxsize)
 NON_CJI = int(11)  # index value of non-cheonjiin input
 AVERAGE_DISTANCE = float(1.61)  # average distance of all possible distance
@@ -41,31 +40,33 @@ def get_phys_dist(origin, typo):
 def calc_edit_dist(str_dict, str_input):
     # "Infinity" -- greater than maximum possible edit distance
     # Used to prevent transpositions for first characters
-    
+
     # table: (M + 2) x (N + 2) sized matrix
-    table = [[INF for _ in range(len(str_input) + 2)] for __ in range(len(str_dict) + 2)]
+    table = [[INF for _ in range(len(str_input)+2)] for __ in range(len(str_dict)+2)]
     table[1][1] = 0
     table[2][1] = AVERAGE_DISTANCE
     table[1][2] = AVERAGE_DISTANCE
-    for i in range(3, len(str_dict) + 2):
-        table[i][1] = table[i - 1][1] + get_phys_dist(str_dict[i - 2], str_dict[i - 3])
-    for i in range(3, len(str_input) + 2):
-        table[1][i] = table[1][i - 1] + get_phys_dist(str_input[i - 2], str_input[i - 3])
+    for i in range(3, len(str_dict)+2):
+        table[i][1] = (table[i-1][1]
+                       + get_phys_dist(str_dict[i-2], str_dict[i-3]))
+    for i in range(3, len(str_input)+2):
+        table[1][i] = (table[1][i-1]
+                       + get_phys_dist(str_input[i-2], str_input[i-3]))
 
     # Holds last row each element was encountered: DA in the Wikipedia pseudocode
     last_row = {}
 
     # Fill in costs
-    for i in range(1, len(str_dict) + 1):
+    for i in range(1, len(str_dict)+1):
         # Current character in a
-        ch_str_dict = str_dict[i - 1]
+        ch_str_dict = str_dict[i-1]
 
         # Column of last match on this row: DB in pseudocode
         last_match_col = 0
 
-        for j in range(1, len(str_input) + 1):
+        for j in range(1, len(str_input)+1):
             # Current character in b
-            ch_str_input = str_input[j - 1]
+            ch_str_input = str_input[j-1]
 
             # Last row with matching character
             last_match_row = last_row.get(ch_str_input, 0)
@@ -76,17 +77,23 @@ def calc_edit_dist(str_dict, str_input):
                 cost = get_phys_dist(ch_str_dict, ch_str_input)
 
             # Compute substring distance
-            val_sub = table[i][j] + cost    # Substitution
-            val_add = (table[i + 1][j]      # Addition
-                       + (table[1][j + 1] - table[1][j]))   # distance from previous
-            val_del = (table[i][j + 1]      # Deletion
-                       + float(1))          # previous: (table[i + 1][1] - table[i][1])
-            val_trs = (table[last_match_row][last_match_col]  # Transposition
-                       + max((i - last_match_row - 1),
-                             (j - last_match_col - 1))
+            # Substitution
+            val_sub = table[i][j] + cost
+            # Addition, distance from previous character
+            val_add = (table[i + 1][j]
+                       + (table[1][j+1] - table[1][j]))
+            # Deletion, doesn't use physical distance
+            val_del = (table[i][j+1]
                        + float(1))
-            table[i + 1][j + 1] = min(val_sub, val_add,
-                                      val_del, val_trs)
+            # Transposition
+            val_trs = (table[last_match_row][last_match_col]
+                       + max((i-last_match_row-1),
+                             (j-last_match_col-1))
+                       + float(1))
+
+            # determining value of current cell
+            table[i+1][j+1] = min(val_sub, val_add,
+                                  val_del, val_trs)
 
             # If there was a match, update last_match_col
             if cost == 0:
@@ -97,4 +104,3 @@ def calc_edit_dist(str_dict, str_input):
 
     # Return last element
     return table[-1][-1]
-
